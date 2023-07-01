c37b674539b6ea798fddc8fad90318ee16ec2bcd
Angel Chapulin <esparquilin2@gmail.com>
2023-07-01 15:27:59 -0600

    First commit

---
+++ src/components/Projects.js
@@ -0,0 +1,119 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+import projImg4 from "../assets/img/project-img4.png";
+import projImg5 from "../assets/img/project-img5.png";
+import projImg6 from "../assets/img/project-img6.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import "animate.css";
+import TrackVisibility from "react-on-screen";
+
+export const Projects = () => {
+  const projects = [
+    {
+      title: "Angel Chapulin Webpage",
+      description: "An alternative",
+      imgUrl: projImg1,
+      link: "https://angel-chapulin-webpage.web.app/",
+    },
+    {
+      title: "Dog Shelter",
+      description: "Available only in Spanish",
+      imgUrl: projImg2,
+      link: "https://refugiocaninomimejoramigo.org/",
+    },
+    {
+      title: "Flower Shop",
+      description: "Beautifull flowers!",
+      imgUrl: projImg3,
+      link: "https://refugiocaninomimejoramigo.org/",
+    },
+  ];
+  const mobileProjects = [
+    {
+      title: "Dog shelter app",
+      description: "Private app ):",
+      imgUrl: projImg4,
+      link: "/",
+    },
+    {
+      title: "English dictionary",
+      description: "Search any word!",
+      imgUrl: projImg5,
+      link: "/",
+    },
+    {
+      title: "Procrastination",
+      description: "On progress...",
+      imgUrl: projImg6,
+      link: "/",
+    },
+  ];
+
+  return (
+    <section className="project" id="project">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
+                  <h2>Projects</h2>
+                  <p>Check out my latest projects!</p>
+                  <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                    <Nav
+                      variant="pills"
+                      className="nav-pills mb-5 justify-content-center align-items-center"
+                      id="pills-tab"
+                    >
+                      <Nav.Item>
+                        <Nav.Link eventKey="first">Web</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="second">Mobile</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="third">More</Nav.Link>
+                      </Nav.Item>
+                    </Nav>
+                    <Tab.Content
+                      id="slideInUp"
+                      className={
+                        isVisible ? "animate__animated animate__slideInUp" : ""
+                      }
+                    >
+                      <Tab.Pane eventKey="first">
+                        <Row>
+                          {projects.map((project, index) => {
+                            return <ProjectCard key={index} {...project} />;
+                          })}
+                        </Row>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="second">
+                        <Row>
+                          {mobileProjects.map((project, index) => {
+                            return <ProjectCard key={index} {...project} />;
+                          })}
+                        </Row>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="third">
+                        <p>In progress...</p>
+                      </Tab.Pane>
+                    </Tab.Content>
+                  </Tab.Container>
+                </div>
+              )}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  );
+};
